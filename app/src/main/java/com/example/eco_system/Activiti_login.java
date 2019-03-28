913328b7e584c8c8eb299470b8e30bf33a68b2cf
EdgarDominguezRamos <1717110264@utectulancingo.edu.mx>
2019-03-28 10:12:56 -0600

    interfaz login terminada

---
+++ app/src/main/java/com/example/eco_system/Activiti_login.java
@@ -0,0 +1,13 @@
+package com.example.eco_system;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activiti_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activiti_login);
+    }
+}
